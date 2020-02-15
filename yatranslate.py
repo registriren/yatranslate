4a93572fa0fb74c540645ac9dcba8831e593cf44
registriren <emailresurs@gmail.com>
2020-02-15 22:56:41 +1000

    add database update

--- yatranslate.py
+++ yatranslate.py
@@ -97,97 +97,102 @@ def translate(text, lang):
 # @app.route('/', methods=['POST'])  # для webhook
 def main():
     res_len = 0
-    last_update = bot.get_updates()
-    # last_update = request.get_json()  # для webhook
-    if last_update:
-        chat_id = bot.get_chat_id(last_update)
-        bot.mark_seen(chat_id)
-        type_upd = bot.get_update_type(last_update)
-        text = bot.get_text(last_update)
-        payload = bot.get_payload(last_update)
-        mid = bot.get_message_id(last_update)
-        callback_id = bot.get_callback_id(last_update)
-        name = bot.get_name(last_update)
-        admins = bot.get_chat_admins(chat_id)
-        if not admins or admins and name in [i['name'] for i in admins['members']]:
-            if text == '/lang' or text == '@yatranslate /lang':
-                buttons = [[{"type": 'callback',
-                             "text": 'Авто|Auto',
-                             "payload": 'auto'},
-                            {"type": 'callback',
-                             "text": 'Русский',
-                             "payload": 'ru'},
-                            {"type": 'callback',
-                             "text": 'English',
-                             "payload": 'en'}]]
-                bot.send_buttons('Направление перевода\nTranslation direction', buttons,
-                                 chat_id)  # вызываем три кнопки с одним описанием
-                text = None
-            if text == '/lang ru' or text == '@yatranslate /lang ru':
-                set_lang('ru', chat_id)
-                bot.send_message('Текст будет переводиться на Русский', chat_id)
-                text = None
-            if text == '/lang en' or text == '@yatranslate /lang en':
-                set_lang('en', chat_id)
-                bot.send_message('Text will be translated into English', chat_id)
-                text = None
-            if text == '/lang auto' or text == '@yatranslate /lang auto':
-                set_lang('auto', chat_id)
-                bot.send_message('Русский|English - автоматически|automatically', chat_id)
-                text = None
-            if payload:
-                set_lang(payload, chat_id)
-                lang = get_lang(chat_id)
-                text = None
-                if lang == 'ru':
-                    bot.send_answer_callback(callback_id, 'Текст будет переводиться на Русский')
-                    bot.delete_message(mid)
-                elif lang == 'auto':
-                    bot.send_answer_callback(callback_id, 'Русский|English - автоматически|automatically')
-                    bot.delete_message(mid)
-                else:
-                    bot.send_answer_callback(callback_id, 'Text will be translated into English')
-                    bot.delete_message(mid)
+    while True:
+        last_update = bot.get_updates()
+        # last_update = request.get_json()  # для webhook
+        if last_update:
+            chat_id = bot.get_chat_id(last_update)
+            bot.mark_seen(chat_id)
+            type_upd = bot.get_update_type(last_update)
+            text = bot.get_text(last_update)
+            payload = bot.get_payload(last_update)
+            mid = bot.get_message_id(last_update)
+            callback_id = bot.get_callback_id(last_update)
+            name = bot.get_name(last_update)
+            admins = bot.get_chat_admins(chat_id)
+            if not admins or admins and name in [i['name'] for i in admins['members']]:
+                if text == '/lang' or text == '@yatranslate /lang':
+                    buttons = [[{"type": 'callback',
+                                 "text": 'Авто|Auto',
+                                 "payload": 'auto'},
+                                {"type": 'callback',
+                                 "text": 'Русский',
+                                 "payload": 'ru'},
+                                {"type": 'callback',
+                                 "text": 'English',
+                                 "payload": 'en'}]]
+                    bot.send_buttons('Направление перевода\nTranslation direction', buttons,
+                                     chat_id)  # вызываем три кнопки с одним описанием
+                    text = None
+                if text == '/lang ru' or text == '@yatranslate /lang ru':
+                    set_lang('ru', chat_id)
+                    bot.send_message('Текст будет переводиться на Русский', chat_id)
+                    text = None
+                if text == '/lang en' or text == '@yatranslate /lang en':
+                    set_lang('en', chat_id)
+                    bot.send_message('Text will be translated into English', chat_id)
+                    text = None
+                if text == '/lang auto' or text == '@yatranslate /lang auto':
+                    set_lang('auto', chat_id)
+                    bot.send_message('Русский|English - автоматически|automatically', chat_id)
+                    text = None
+                if payload:
+                    set_lang(payload, chat_id)
+                    lang = get_lang(chat_id)
+                    text = None
+                    if lang == 'ru':
+                        bot.send_answer_callback(callback_id, 'Текст будет переводиться на Русский')
+                        bot.delete_message(mid)
+                    elif lang == 'auto':
+                        bot.send_answer_callback(callback_id, 'Русский|English - автоматически|automatically')
+                        bot.delete_message(mid)
+                    else:
+                        bot.send_answer_callback(callback_id, 'Text will be translated into English')
+                        bot.delete_message(mid)
 
-        if type_upd == 'bot_started':
-            bot.send_message(
-                'Отправьте или перешлите боту текст. Язык переводимого текста определяется автоматически. '
-                'Перевод по умолчанию на русский. Для изменения направления перевода используйте команду /lang',
-                chat_id)
-            set_lang('ru', chat_id)
-            text = None
-        if chat_id:
-            lang = get_lang(chat_id)
-        elif '-' in str(chat_id):
-            lang = 'ru'
-        else:
-            lang = 'auto'
-        if type_upd == 'message_construction_request':
-            text_const = bot.get_construct_text(last_update)
-            sid = bot.get_session_id(last_update)
-            if text_const:
-                translt = translate(text_const, 'auto')
-                if translt:
-                    bot.send_construct_message(sid, hint=None, text=translt)
+            if type_upd == 'bot_started':
+                bot.send_message(
+                    'Отправьте или перешлите боту текст. Язык переводимого текста определяется автоматически. '
+                    'Перевод по умолчанию на русский. Для изменения направления перевода используйте команду /lang',
+                    chat_id)
+                set_lang('auto', chat_id)
+                text = None
+            if chat_id:
+                lang = get_lang(chat_id)
+                if not lang and '-' in str(chat_id):
+                    lang = 'ru'
+                    set_lang('ru', chat_id)
+                elif not lang:
+                    lang = 'auto'
+                    set_lang('auto', chat_id)
+            else:
+                lang = 'auto'
+            if type_upd == 'message_construction_request':
+                text_const = bot.get_construct_text(last_update)
+                sid = bot.get_session_id(last_update)
+                if text_const:
+                    translt = translate(text_const, 'auto')
+                    if translt:
+                        bot.send_construct_message(sid, hint=None, text=translt)
+                    else:
+                        bot.send_construct_message(sid, 'Введите текст для перевода и отправки в чат | '
+                                                        'Enter the text to be translated and send to the chat')
                 else:
                     bot.send_construct_message(sid, 'Введите текст для перевода и отправки в чат | '
                                                     'Enter the text to be translated and send to the chat')
-            else:
-                bot.send_construct_message(sid, 'Введите текст для перевода и отправки в чат | '
-                                                'Enter the text to be translated and send to the chat')
-        elif text:
-            translt = translate(text, lang)
-            if translt:
-                len_sym = len(translt)
-                res_len += len_sym
-                logger.info('chat_id: {}, len symbols: {}, result {}'.format(chat_id, len_sym, res_len))
-                if res_len >> 10000000:  # контроль в логах количества переведенных символов
-                    res_len = 0
-                if '-' in str(chat_id):
-                    bot.send_reply_message(translt, mid, chat_id)
-                else:
-                    bot.send_message(translt, chat_id)
-    # return jsonify(last_update)  # для webhook
+            elif text:
+                translt = translate(text, lang)
+                if translt:
+                    len_sym = len(translt)
+                    res_len += len_sym
+                    logger.info('chat_id: {}, len symbols: {}, result {}'.format(chat_id, len_sym, res_len))
+                    if res_len >> 10000000:  # контроль в логах количества переведенных символов
+                        res_len = 0
+                    if '-' in str(chat_id):
+                        bot.send_reply_message(translt, mid, chat_id)
+                    else:
+                        bot.send_message(translt, chat_id)
+        # return jsonify(last_update)  # для webhook
 
 
 # if __name__ == '__main__':  # для webhook
@@ -198,7 +203,6 @@ def main():
 
 if __name__ == '__main__':
     try:
-        while True:
-            main()
+        main()
     except KeyboardInterrupt:
         exit()
